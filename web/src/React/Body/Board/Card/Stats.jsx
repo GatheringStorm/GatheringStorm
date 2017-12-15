dbb55c3ad426b1706cc31dd11849e27c6ae9d699
Fabian <fabi.amhof@bluewin.ch>
2017-12-15 15:13:56 +0100

    css

--- web/src/React/Body/Board/Card/Stats.jsx
+++ web/src/React/Body/Board/Card/Stats.jsx
@@ -9,8 +9,8 @@ class Stats extends React.Component {
     render() {
         return (
             <div>
-                <div className="stats">{this.props.attack}</div>
-                <div className="stats">{this.props.hp}</div>
+                <label className="stats col-md-1 col-lg-1 col-sm-1 col-xs-1">{this.props.attack}</label>
+                <label className="stats col-md-1 col-md-offset-10 col-lg-1 col-lg-offset-10 col-sm-1 col-sm-offset-10 col-xs-1 col-xs-offset-10">{this.props.hp}</label>
             </div>
         );
     }
