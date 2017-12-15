c217e6b8851c907cd5c641c78313517ac527fe41
Fabian <fabi.amhof@bluewin.ch>
2017-12-15 20:06:15 +0100

    css error fixed

--- web/src/React/Body/GameSelection/Body/Body.jsx
+++ web/src/React/Body/GameSelection/Body/Body.jsx
@@ -22,7 +22,7 @@ class Body extends React.Component {
         let testGames = [{ mode: "new", opponent: { email: "" }, beginDate: "" }];
         testGames.push(...await this.getOpenGames());
         let obj = (
-            <div className="flex-container-horizontal">{
+            <div className="flex-container-horizontal-wrap">{
                 testGames.map((item, index) => {
                     return <GameSelectionCard key={index} stateMachine={this.props.stateMachine} mode={item.mode} email={item.opponent.email} date={item.beginDate} currentTurnPlayer={item.currentTurnPlayer} />
                 })
