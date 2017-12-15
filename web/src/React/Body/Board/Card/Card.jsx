a53067563ec4f0542875631843cf56d62261ea22
Fabian <fabi.amhof@bluewin.ch>
2017-12-15 11:02:59 +0100

    added Card

---
+++ web/src/React/Body/Board/Card/Card.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+import Stats from "./Stats.jsx";
+
+class Card extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { effects: null }
+  }
+
+  componentDidMount() {
+    let effect = [];
+    effect.push(...this.props.card.effects);
+    let effects = (
+      <div>{
+        effect.map((item, index) => {
+          return <p key={index}>{item.description}</p>
+        })
+      }</div>
+    )
+
+    this.setState({
+      effects: effects
+    })
+  }
+
+  render() {
+    console.log(this.props.card)
+    return (
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <div>
+                <h3>{this.props.card.name}</h3>
+                <p>{this.props.card.cost}</p>
+              </div>
+              <p>{this.props.card.title}</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              {this.state.effects}
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <Stats attack={this.props.card.attack} hp={this.props.card.health} />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default Card;
