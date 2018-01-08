52b02abbe59500c73e90f9d3d85f675846f518e1
Lukas Dörig <lukee910@gmail.com>
2018-01-08 12:46:52 +0900

    Add Cost to DtoCard

--- api/GatheringStorm.Api/Models/Dto/DtoCard.cs
+++ api/GatheringStorm.Api/Models/Dto/DtoCard.cs
@@ -8,6 +8,7 @@ namespace GatheringStorm.Api.Models.Dto
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Title { get; set; }
+        public int Cost { get; set; }
         public int Attack { get; set; }
         public int Health { get; set; }
         public int StatsModifiersCount { get; set; }
