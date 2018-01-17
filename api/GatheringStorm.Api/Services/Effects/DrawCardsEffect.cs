45d29e9ba40605009657c48cf3ac12a9f6bd9a5b
Lukas Dörig <lukee910@gmail.com>
2018-01-17 21:37:01 +0100

    Implement DrawCardsEffect

--- api/GatheringStorm.Api/Services/Effects/DrawCardsEffect.cs
+++ api/GatheringStorm.Api/Services/Effects/DrawCardsEffect.cs
@@ -3,7 +3,9 @@ using System.Threading.Tasks;
 using GatheringStorm.Api.Data;
 using GatheringStorm.Api.Models;
 using GatheringStorm.Api.Models.DB;
+using GatheringStorm.Api.Models.DB.Effects;
 using GatheringStorm.Api.Models.Dto;
+using Newtonsoft.Json;
 
 namespace GatheringStorm.Api.Services.Effects
 {
@@ -14,21 +16,31 @@ namespace GatheringStorm.Api.Services.Effects
     public class DrawCardsEffect : IDrawCardsEffect
     {
         private readonly AppDbContext dbContext;
+        private readonly ICardService cardService;
 
-        public DrawCardsEffect(AppDbContext dbContext)
+        public DrawCardsEffect(AppDbContext dbContext, ICardService cardService)
         {
+            this.cardService = cardService;
             this.dbContext = dbContext;
 
         }
 
-        public Task<VoidAppResult> ConfigureDtoEffect(CardEffect cardEffect, DtoEffect dtoEffect)
+        public async Task<VoidAppResult> ExecuteEffect(DtoEffectTargets effect, CardEffect cardEffect, Game game, User currentTurnPlayer, CancellationToken cancellationToken = default(CancellationToken))
         {
-            throw new System.NotImplementedException();
+            var parameters = JsonConvert.DeserializeObject<DrawCardsEffectParameters>(cardEffect.EffectParameters);
+
+            return await this.cardService.DrawCard(game, currentTurnPlayer.Mail, parameters.CardsCount);
         }
 
-        public Task<VoidAppResult> ExecuteEffect(DtoEffectTargets effect, CardEffect cardEffect, Game game, User currentTurnPlayer, CancellationToken cancellationToken = default(CancellationToken))
+        public Task<VoidAppResult> ConfigureDtoEffect(CardEffect cardEffect, DtoEffect dtoEffect)
         {
-            throw new System.NotImplementedException();
+            var parameters = JsonConvert.DeserializeObject<DrawCardsEffectParameters>(cardEffect.EffectParameters);
+
+            dtoEffect.Name = "Draw cards";
+            dtoEffect.TargetsCount = 0;
+            dtoEffect.Description = $"Draw {parameters.CardsCount} cards.";
+
+            return Task.FromResult(VoidAppResult.Success());
         }
     }
 }
